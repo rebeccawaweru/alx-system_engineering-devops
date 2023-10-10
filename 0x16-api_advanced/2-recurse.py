e05b6f52fb3b22714a515c63c205ce436793a6aa
rebeccawaweru <wawerur95@gmail.com>
2023-10-10 19:37:30 +0300

    updatess

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,28 +3,27 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None, count=0):
     """ Get hot posts
         Args:
             subreddit: name of subreddit
             host_list: title list
             after: next results id
     """
-    url = 'https://api.reddit.com/r/'
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    response = requests.get(
-        '{}{}/hot?after={}'.format(
-            url, subreddit, after), headers=headers, allow_redirects=False)
+    response = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if response.status_code != 200:
         return None
-    else:
-        dict_hot = response.json()
-        if len(dict_hot['data']['children']) == 0:
-            return hot_list
-        else:
-            for x in dict_hot['data']['children']:
-                hot_list.append(x['data']['title'])
-            after = dict_hot['data']['after']
-            if after is None:
-                return hot_list
-            return recurse(subreddit, hot_list, after=after)
+    hot_dict = hot_list + [child.get("data").get("title")
+                           for child in response.json()
+                           .get("data")
+                           .get("children")]
+    info = response.json()
+    if not info.get("data").get("after"):
+        return hot_dict
+
+    return recurse(subreddit, hot_dict, info.get("data").get("count"),
+                   info.get("data").get("after"))
