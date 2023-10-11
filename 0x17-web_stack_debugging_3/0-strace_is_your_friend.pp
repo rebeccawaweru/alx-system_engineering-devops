e55979956611b4eb832d0219d079b22e107a81ab
rebeccawaweru <wawerur95@gmail.com>
2023-10-11 14:57:35 +0300

    updates

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fix bad 'phpp' extension to 'php' in wordpress file
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path	  => '/usr/local/bin/:/bin/'
+# correction
+exec {'correct':
+  command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+  path    => '/bin',
 }
